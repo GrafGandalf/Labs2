3b0fb7e84fc1e1a7bc069358c9198a2943457b93
Ivan <enileev.ivann@gmail.com>
2022-06-21 13:44:09 +0200

    Build Final

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,55 +1,163 @@
-#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <chrono> // для функций из std::chrono
+ 
+class Timer
+{
+private:
+    // Псевдонимы типов используются для удобного доступа к вложенным типам
+    using clock_t = std::chrono::high_resolution_clock;
+    using second_t = std::chrono::duration<double, std::ratio<1> >;
+    
+    std::chrono::time_point<clock_t> m_beg;
+ 
+public:
+    Timer() : m_beg(clock_t::now())
+    {
+    }
+    
+    void reset()
+    {
+        m_beg = clock_t::now();
+    }
+    
+    double elapsed() const
+    {
+        return std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();
+    }
+};
+
+int Sort1 (int Mass[], int Nreal, int N)
+{
+    for (int i=0; i<Nreal; i++)
+    {
+        if (Mass[i]%2==0)
+        {
+            for (int j=Nreal; j>i; j--)
+            {
+                std::swap(Mass[j], Mass[j-1]);
+            }
+            Mass[i]=Mass[i+1];
+            i++; Nreal++;
+        }
+    }
+    
+    return Nreal;
+}
+int Sort2 (int Mass[], int Nreal, int N)
+{
+    for (int i=0; i<Nreal; i++)
+    {
+        if (Mass[i]%2==0)
+        {
+            int *MassZ = new int[Nreal+1];
+            
+            for (int j=0; j<Nreal+1; j++)
+            {
+                if (j<i) MassZ[j]=Mass[j];
+                if ((j==i)||(j==i+1)) MassZ[j]=Mass[j];
+                if (j>i) MassZ[j]=Mass[j-1];
+            }
+            i++; Nreal++;
+            for (int j=0; j<Nreal; j++)
+            {
+                Mass[j]=MassZ[j];
+            }
+            delete [] MassZ;
+        }
+    }
+    
+    return Nreal;
+}
+int Sort3 (int Mass[], int Nreal, int N)
+{
+    int *MassZ = new int[N];
+    for (int j=0; j<Nreal; j++)
+    {
+        MassZ[2*j]=Mass[j];
+        MassZ[2*j+1]=Mass[j];
+    }
+    Nreal = N;
+    
+    for (int i=0; i<Nreal; i++)
+    {
+        
+        if (Mass[i]%2==1)
+        {
+            for (int j=i; j<Nreal*2-1; j++)
+            {
+                Mass[j]=Mass[j+1];
+            }
+            Nreal--;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    
+    for (int j=0; j<Nreal; j++)
+    {
+        Mass[j]=MassZ[j];
+    }
+    
+    delete [] MassZ;
+    return Nreal;
+}
+
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(2560, 1400), "SFML works!");
+    int N=0; int Nreal=0;
+    std::cin>>N; Nreal=N; N=N*2;
+    int NZ=N; int NrealZ=Nreal;
     
-    sf::RectangleShape shape(sf::Vector2f(500.f, 230.f));
-    shape.setOutlineColor(sf::Color::Red);
-    shape.setOutlineThickness(5);
-    shape.setPosition(1280, 700);
-    shape.setOrigin(250,115);
-    shape.setFillColor(sf::Color(255, 0, 128));
+    int *Mass = new int[N];
+    int *MassZamen = new int[Nreal];
     
-    sf::CircleShape shape1(150.f);
-    shape1.setFillColor(sf::Color(128, 128, 128));
-    shape1.setPosition(0, 0);
-    shape1.setOrigin(75,75);
-    
-    sf::ConvexShape polygon;
-    polygon.setPointCount(3);
-    polygon.setPoint(0, sf::Vector2f(100, 0));
-    polygon.setPoint(1, sf::Vector2f(10, 70));
-    polygon.setPoint(2, sf::Vector2f(190, 70));
-    polygon.setOutlineColor(sf::Color::Red);
-    polygon.setOutlineThickness(5);
-    polygon.setPosition(2560-200, 100);
-    
-    int Cvad1_y=0, Cvad_y=0, Cvad2_y=0;
-
-    while (window.isOpen())
+    for (int i=0; i<N; i++)
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        
-        if (Cvad_y<700-115-5) Cvad_y++;
-        if (Cvad1_y<1400-225) Cvad1_y++;
-        if (Cvad2_y<1400-100-70) Cvad2_y++;
-        
-        shape.setPosition(1280, 700+Cvad_y);
-        shape1.setPosition(0+Cvad1_y, 0+Cvad1_y);
-        polygon.setPosition(2560-200-Cvad2_y, 100+Cvad2_y);
-
-        window.clear();
-        window.draw(shape);
-        window.draw(shape1);
-        window.draw(polygon);
-        window.display();
+        if (i<Nreal) Mass[i]=rand()%10+1;
+        else Mass[i]=0;
+        MassZamen[i]=Mass[i];
     }
-
+    
+    std::cout << "Alg1:" << std::endl;
+    
+    Timer t1;
+    
+    Nreal = Sort1(Mass, Nreal, N);
+    
+    std::cout << "Time:"<< t1.elapsed() << std::endl;
+    
+    N=NZ; Nreal=NrealZ;
+    for (int i=0; i<N; i++)
+    {
+        Mass[i]=MassZamen[i];
+    }
+    
+    std::cout << "Alg2:" << std::endl;
+    
+    Timer t2;
+    
+    Nreal = Sort2(Mass, Nreal, N);
+    
+    std::cout << "Time:"<< t2.elapsed() << std::endl;
+    
+    N=NZ; Nreal=NrealZ;
+    for (int i=0; i<N; i++)
+    {
+        Mass[i]=MassZamen[i];
+    }
+    
+    std::cout << "Alg3:" << std::endl;
+    
+    Timer t3;
+    
+    Nreal = Sort3(Mass, Nreal, N);
+    
+    std::cout << "Time:"<< t3.elapsed() << std::endl;
+    
+    
+    delete [] Mass; delete [] MassZamen;
     return 0;
 }
